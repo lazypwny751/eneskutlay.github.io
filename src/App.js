1fa0e845a69481615c2d9cf038e549ce5df8cc41
eneskt <eneskt1@hotmail.com>
2020-11-30 17:01:33 +0300

    personal website completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TypeIt from "typeit-react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containerMessage">
+      <div className="bar">
+        <span className="icon icon-red"></span>
+        <span className="icon icon-yellow"></span>
+        <span className="icon icon-green"></span>
+      </div>
+      <div className="screen">
+        <TypeIt
+          options={{ speed: 100 }}
+          getBeforeInit={(instance) => {
+            instance
+              .break()
+              .type("Hey... Hello sir 👋 ", { delay: 200 })
+              .pause(500)
+              .break({ delay: 500 })
+              .type("<strong> I'm Enes. </strong>")
+              .break({ delay: 200 })
+              .break({ delay: 200 })
+              .type("I'm a university student")
+              .break()
+              .type("and learn Front-End development")
+              .delete(27, { delay: 400 })
+              .type("Front-End developer. 💻 ")
+              .break()
+              .break()
+              .type(
+                "İf you want to contact me ?<br> <a href='mailto:eneskt1@hotmail.com'>eneskt1@hotmail.com</a> <i> you can disturb me 👌</i>"
+              )
+              .break()
+              .break()
+              .type(
+                "İf you wonder about me ?<br> <a href='https://github.com/eneskt' target=_blank>GitHub Page</a> <br> <a href='https://www.linkedin.com/in/enes-kutlay-4038b01a0/' target=_blank>LinkedIn Profile </a> <br> <a href='https://twitter.com/eneskt01' target=_blank>Twitter Profile <br> <a href='https://enes-kt.medium.com/' target=_blank>Medium Blog Page</a></a>"
+              )
+              .break()
+              .break()
+              .type("Have good day 🎉 ");
+
+            return instance;
+          }}
+        />
+      </div>
     </div>
   );
-}
-
-export default App;
+};
